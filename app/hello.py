fb55976573728148d1f51afdd5788412df2555a0
ilyaro <astra07_2010@yahoo.com>
2020-10-30 23:58:03 +0200

    Autocommit...

--- app/hello.py
+++ app/hello.py
@@ -39,7 +39,8 @@ def pingpong():
 	iterations_int = int(iterations)
 	##Get timeout optional argument
 	timeout = request.args.get('timeout', None)
-	timeout_int = int(timeout)
+	if timeout:
+		timeout_int = int(timeout)
 	iterations_count = 1
 	inport_str = str(inport)
 	myhostname = os.getenv('HOSTNAME')
@@ -61,12 +62,10 @@ def pingpong():
 			b = datetime.now()
 			c = b - a
 			##Check if timeout is reached
-			curr_whole_time = round((b - start_time).total_seconds()) * 1000
-			##round(curr_whole_time.total_seconds() * 1000)
-			if timeout:
-				if curr_whole_time > timeout_int:
-					response = print_message(" Game Over. Timeout: " + timeout + " miliseconds " + " is reached. " + "The Game time is " + str(curr_whole_time) + " miliseconds ", "") 
-					return response
+			curr_whole_time = round((b - start_time).total_seconds() * 1000)
+			if curr_whole_time > timeout_int:
+				response = print_message(" Game Over. Timeout: " + timeout + " miliseconds " + " is reached. " + "The Game time is " + str(curr_whole_time) + " miliseconds ", "") 
+				return response
 		except URLError as e:
 			response = print_message(" Unknown - service problem: Cant reach server: " + url_pong + " ", iterations_count_str) 
 			return response
@@ -74,7 +73,7 @@ def pingpong():
 		iterations_count += 1
 	finish_time = datetime.now()
 	c = finish_time - start_time
-	fin_message = " Game Over, took " + str(round(c.total_seconds() * 1000, 3))[:-3] + " ms"
+	fin_message = " Game Over, took " + str(round(c.total_seconds() * 1000, 3)) + " ms"
 	print_message(fin_message, "")
 	return fin_message + " See docker compose output for datails"
 
